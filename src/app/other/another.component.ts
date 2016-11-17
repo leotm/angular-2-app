362fe19bb1b9245441ce2fea46df9005b3317aa5
LeoTM <leo.m@hotmail.co.uk>
2016-11-17 20:03:00 +0000

    Try <ng-content>
    Note: app component styling applied.

--- src/app/other/another.component.ts
+++ src/app/other/another.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-another',
   template: `
-    <p>
-      another Works!
-    </p>
+    <ng-content></ng-content>
   `
 })
 export class AnotherComponent {
