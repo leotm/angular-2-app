9d48abdc00ceb0ad96ffe46cd26c9694f8e1afe4
LeoTM <leo.m@hotmail.co.uk>
2016-11-17 17:46:26 +0000

    Add moduleId

--- src/app/other/other.component.ts
+++ src/app/other/other.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  moduleId: 'module.id',
   selector: 'app-other',
   templateUrl: './other.component.html',
   styleUrls: ['./other.component.css']
