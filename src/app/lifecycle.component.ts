ff0d6f4cb0320ecdb6123e52c5abe61c7eaf3691
LeoTM <leo.m@hotmail.co.uk>
2016-11-19 15:10:18 +0000

    Implement hook interfaces

--- src/app/lifecycle.component.ts
+++ src/app/lifecycle.component.ts
@@ -9,11 +9,45 @@ import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentCh
   `,
   styles: []
 })
-export class LifecycleComponent implements OnInit {
+export class LifecycleComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+  constructor() {
+  }
+
+  ngOnChanges() {
+    this.log('ngOnChanges');
+  }
 
   ngOnInit() {
+    this.log('ngOnInit');
+  }
+
+  ngDoCheck() {
+    this.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    this.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    this.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    this.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    this.log('ngAfterViewChecked');    
+  }
+
+  ngOnDestroy() {
+    this.log('ngOnDestroy');
+  }
+
+  private log(hook: string) {
+    console.log(hook);
   }
 
 }
