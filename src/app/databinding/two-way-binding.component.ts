30293fe63227d556312b86bf510cac180044714b
LeoTM <leo.m@hotmail.co.uk>
2016-11-19 14:23:53 +0000

    Try [(ngModel)] directive

--- src/app/databinding/two-way-binding.component.ts
+++ src/app/databinding/two-way-binding.component.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-two-way-binding',
   template: `
-    <p>
-      two-way-binding Works!
-    </p>
+    <input type="text" [(ngModel)]="person.name">
+    <input type="text" [(ngModel)]="person.name">
   `,
   styles: []
 })
 export class TwoWayBindingComponent {
-
+  person = {
+    name: 'leo',
+    age: 30
+  };
 }
