72e10d28f7b003ba3fa6e8a9ea97016295a87611
LeoTM <leo.m@hotmail.co.uk>
2016-11-19 12:56:20 +0000

    Remove OnInit and constructor

--- src/app/databinding/two-way-binding.component.ts
+++ src/app/databinding/two-way-binding.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-two-way-binding',
@@ -9,11 +9,6 @@ import { Component, OnInit } from '@angular/core';
   `,
   styles: []
 })
-export class TwoWayBindingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class TwoWayBindingComponent {
 
 }
